98067fab8ce9dc5df8253759d471214c5a38dd86
Lucien Boudy <luboudy@gmail.com>
2016-07-05 20:13:30 +0200

    prepare scaling

--- game.gd
+++ game.gd
@@ -6,8 +6,11 @@ extends Node2D
 # var b="textvar"
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	set_process_input(true)
 
 
+func _input(event):
+	if event.is_action_pressed("spell_left"):
+		global.scale_factor = clamp(global.scale_factor + 1, global.SCALE_FACTOR_MIN, global.SCALE_FACTOR_MAX)
+	elif event.is_action_pressed("spell_right"):
+		global.scale_factor = clamp(global.scale_factor - 1, global.SCALE_FACTOR_MIN, global.SCALE_FACTOR_MAX)
--- global.gd
+++ global.gd
@@ -6,5 +6,9 @@ var WALK_SPEED = 100
 const WALK_SPEED_MIN = 5
 const WALK_SPEED_MAX = 7
 
+var scale_factor = 2
+const SCALE_FACTOR_MIN = 0
+const SCALE_FACTOR_MAX = 0
+
 func _ready():
 	pass
