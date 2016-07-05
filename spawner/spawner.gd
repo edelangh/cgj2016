214e81a0180c5d12b96b15ac1ce43b67e6a2e18e
Lucien Boudy <luboudy@gmail.com>
2016-07-05 23:56:01 +0200

    tileset playtesting tool

--- spawner/spawner.gd
+++ spawner/spawner.gd
@@ -1,27 +1,26 @@
 
-extends Control
+extends Node2D
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
+export(String, FILE) var tileset_test = null
 
 var store = []
 var rooms = []
+var room_i = 0
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+	if tileset_test != null:
+		add_child(load(tileset_test).instance())
+		return
 	store.push_back(preload('res://patterns/p_001.tscn'))
 	store.push_back(preload('res://patterns/p_002.tscn'))
 	var room = store[0].instance()
 	add_child(room)
 	rooms.push_back(room)
 	set_process(true)
-	pass
-
-var i = 0
 
 func _process(delta):
+	if tileset_test != null:
+		return
 	if rooms.size() < 3:
 		var room = store[rand_range(0, store.size())].instance()
 		var lol = global.camera_pos.x - fmod(global.camera_pos.x, 64)
@@ -30,7 +29,7 @@ func _process(delta):
 		add_child(room)
 		rooms.push_back(room)
 		print("add")
-	i += 1
+	room_i += 1
 	for room in rooms:
 		var dist = room.get_pos() - global.camera_pos
 		var norm = dist.length()
