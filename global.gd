1700759a1e9bbc68dbbed7214fc2b883e94eaf5d
Lucien Boudy <luboudy@gmail.com>
2016-07-06 17:05:33 +0200

    main feature lol

--- global.gd
+++ global.gd
@@ -1,15 +1,16 @@
 
 extends Node
 
+signal dimension_left(scale)
+signal dimension_right(scale)
+
 var main_camera = null
 var camera_pos = Vector2()
 var WALK_SPEED = 300
 const WALK_SPEED_MIN = 6
 const WALK_SPEED_MAX = 9
 var gameover = false
-var scale_factor = 2
-const SCALE_FACTOR_MIN = 0
-const SCALE_FACTOR_MAX = 0
+var scale_factor = 0
+const SCALE_FACTOR_MIN = -1
+const SCALE_FACTOR_MAX = 1
 
-func _ready():
-	pass
