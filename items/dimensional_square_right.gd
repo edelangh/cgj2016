acae4b5fb3d46955f0522f4f6ac9d30d1b8ccdff
Lucien Boudy <luboudy@gmail.com>
2016-07-07 23:36:30 +0200

    fix

--- items/dimensional_square_right.gd
+++ items/dimensional_square_right.gd
@@ -4,6 +4,7 @@ extends Sprite
 export(int, "Small", "Medium", "Big") var starting_scale = 1
 var current_scale
 
+
 func _ready():
 	var s = [0.25, 0.5, 1][starting_scale]
 	set_scale(Vector2(s, s))
@@ -11,16 +12,20 @@ func _ready():
 	global.connect("dimension_left", self, "on_dimension_left", [])
 	global.connect("dimension_right", self, "on_dimension_right", [])
 
+
 func on_dimension_left(scale):
-	if scale == 0:
+	if current_scale == 1:
 		get_node("anim").play_backwards("right")
-	elif scale != current_scale:
+		current_scale -= 1
+	elif current_scale == 0:
 		get_node("anim").play("left")
-	current_scale = scale
+		current_scale -= 1
+
 
 func on_dimension_right(scale):
-	if scale == 0:
+	if current_scale == -1:
 		get_node("anim").play_backwards("left")
-	elif scale != current_scale:
+		current_scale += 1
+	elif current_scale == 0:
 		get_node("anim").play("right")
-	current_scale = scale
+		current_scale += 1
