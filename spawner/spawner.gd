4a60c0d5573c51d28b982c72f48c8428f55d1674
Lucien Boudy <luboudy@gmail.com>
2016-07-07 19:51:45 +0200

    fix

--- spawner/spawner.gd
+++ spawner/spawner.gd
@@ -9,37 +9,36 @@ var instance_count = 0
 const PATTERNS_WIDTH = 64 * 40
 var start_pos
 
+
 func _ready():
 	start_pos = -get_viewport().get_rect().size.x * 0.5
-	if tileset_test != null:
-		var room = load(tileset_test).instance()
-		room.set_pos(Vector2(start_pos, 0))
-		add_child(room)
-		set_process(true)
-		return
 	store.push_back(preload('res://patterns/p_001.tscn'))
 	store.push_back(preload('res://patterns/p_002.tscn'))
 	store.push_back(preload('res://patterns/p_003.tscn'))
-	var room = store[0].instance()
+	var room
+	if not tileset_test:
+		room = store[0].instance()
+	else:
+		room = load(tileset_test).instance()
 	room.set_pos(Vector2(start_pos, 0))
 	add_child(room)
 	rooms.push_back(room)
 	set_process(true)
 
+
 func _process(delta):
 	if rooms.size() < 3:
 		instance_count += 1
-		if tileset_test != null:
-			var room = load(tileset_test).instance()
-			room.set_pos(Vector2(start_pos + PATTERNS_WIDTH * instance_count, 0))
-			add_child(room)
+		var room
+		if not tileset_test:
+			room = store[rand_range(1, store.size())].instance()
 		else:
-			var room = store[rand_range(1, store.size())].instance()
-			room.set_pos(Vector2(start_pos + PATTERNS_WIDTH * instance_count, 0))
-			add_child(room)
-			rooms.push_back(room)
+			room = load(tileset_test).instance()
+		room.set_pos(Vector2(start_pos + PATTERNS_WIDTH * instance_count, 0))
+		add_child(room)
+		rooms.push_back(room)
 	for room in rooms:
 		var dx = room.get_pos().x - global.main_camera.get_pos().x
-		if dx < start_pos - PATTERNS_WIDTH:
+		if dx < -get_viewport().get_rect().size.x * 0.7 - PATTERNS_WIDTH:
 			rooms.erase(room)
 			room.queue_free()
