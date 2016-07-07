71ca4da4e895a44d300101c15d350770ac71ff83
Lucien Boudy <luboudy@gmail.com>
2016-07-07 19:39:19 +0200

    stuff

--- spawner/spawner.gd
+++ spawner/spawner.gd
@@ -12,9 +12,9 @@ var start_pos
 func _ready():
 	start_pos = -get_viewport().get_rect().size.x * 0.5
 	if tileset_test != null:
-		var pattern = load(tileset_test).instance()
-		pattern.set_pos(Vector2(start_pos, 0))
-		add_child(pattern)
+		var room = load(tileset_test).instance()
+		room.set_pos(Vector2(start_pos, 0))
+		add_child(room)
 		set_process(true)
 		return
 	store.push_back(preload('res://patterns/p_001.tscn'))
@@ -34,12 +34,12 @@ func _process(delta):
 			room.set_pos(Vector2(start_pos + PATTERNS_WIDTH * instance_count, 0))
 			add_child(room)
 		else:
-			var room = store[rand_range(0, store.size())].instance()
+			var room = store[rand_range(1, store.size())].instance()
 			room.set_pos(Vector2(start_pos + PATTERNS_WIDTH * instance_count, 0))
 			add_child(room)
 			rooms.push_back(room)
 	for room in rooms:
-		var dist = room.get_pos() - global.main_camera.get_pos()
-		var len = dist.length()
-		if dist.x < 0 and len > PATTERNS_WIDTH:
+		var dx = room.get_pos().x - global.main_camera.get_pos().x
+		if dx < start_pos - PATTERNS_WIDTH:
 			rooms.erase(room)
+			room.queue_free()
