b42e9f708d50e4027b5e4079b42dcd48a59d07b4
edelangh <edelangh@student.42.fr>
2016-07-08 17:57:54 +0200

    add beer and hotdog

--- items/coin.gd
+++ items/coin.gd
@@ -3,6 +3,8 @@ extends Area2D
 # Member variables
 var taken = false
 var audioPlayer = null
+export(String) var sound = "coin-collect"
+export(int)    var score = 1
 
 func _ready():
 	audioPlayer = get_node("audioPlayer")
@@ -10,8 +12,8 @@ func _ready():
 
 func _on_Area2D_body_enter(body):
 	if not taken and body extends preload("res://player/player.gd"):
-		audioPlayer.play("coin-collect")
+		audioPlayer.play(sound)
 		get_node("anim").play("taken")
 		taken = true
-		global.score_multiplier += 1
+		global.score_multiplier += score
 		global.score_multiplier_timer = global.SCORE_MULTIPLIER_DURATION
