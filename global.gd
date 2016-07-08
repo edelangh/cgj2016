1cbc54381397fa3ad800434973e2250607874369
Lucien Boudy <luboudy@gmail.com>
2016-07-08 10:15:00 +0200

    stuff

--- global.gd
+++ global.gd
@@ -11,7 +11,6 @@ var highscore = 0
 var score_multiplier = 1
 var score_multiplier_timer = 0
 var main_camera = null
-#var camera_pos = Vector2()
 var player
 var WALK_SPEED = 1000
 const WALK_SPEED_MIN = 350
@@ -20,7 +19,7 @@ var gameover = false
 var scale_factor = 0
 const SCALE_FACTOR_MIN = -1
 const SCALE_FACTOR_MAX = 1
-const PLAYER_START_POS = Vector2(-500, 300)
+const PLAYER_START_POS = Vector2(-500, 0)
 
 func run_death():
 	if not gameover:
