d238dc150138ee4a614bc3cd087edf0dcb197f51
edelangh <edelangh@student.42.fr>
2016-07-08 17:14:13 +0200

    add sounds

--- items/coin.gd
+++ items/coin.gd
@@ -1,16 +1,16 @@
-
 extends Area2D
-
 
 # Member variables
 var taken = false
+var audioPlayer = null
 
 func _ready():
+	audioPlayer = get_node("audioPlayer")
 	connect("body_enter", self, "_on_Area2D_body_enter")
 
 func _on_Area2D_body_enter(body):
 	if not taken and body extends preload("res://player/player.gd"):
-		get_node("SamplePlayer2D").play("coin-collect")
+		audioPlayer.play("coin-collect")
 		get_node("anim").play("taken")
 		taken = true
 		global.score_multiplier += 1
